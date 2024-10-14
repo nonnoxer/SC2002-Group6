8dea14b2966b1ba751ace095b120cfb745ca5eba
Frost1432 <103715831+frost312@users.noreply.github.com>
2024-10-14 21:34:12 +0800

    Update User.java

--- user/User.java
+++ user/User.java
@@ -35,36 +35,4 @@ public class User {
         this.password = password;
     }
 
-    // Save updated credentials to the respective CSV file
-    public void saveToCSV() {
-        String fileName = "";
-        switch (role.toLowerCase()) {
-            case "doctor":
-                fileName = "Staff_List.csv";
-                break;
-            case "patient":
-                fileName = "Patient_List.csv";
-                break;
-            case "pharmacist":
-                fileName = "Staff_List.csv";
-                break;
-            case "administrator":
-                fileName = "Staff_List.csv";
-                break;
-            default:
-                System.out.println("Unknown role: Cannot save to CSV.");
-                return;
-        }
-
-        //Create a new object writer in order to modify the CSV files
-        try (FileWriter writer = new FileWriter(fileName, true)) {
-            writer.append(id).append(",");
-            writer.append(password).append(",");
-            writer.append(name).append(",");
-            writer.append(role).append("\n");
-            System.out.println("Updated credentials saved to " + fileName);
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage()); //catch any errors or unappropriate requests and return an error message
-        }
-    }
 }
