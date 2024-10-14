8618db52359db2dd859d2dd9b414479f14e5c939
Frost1432 <103715831+frost312@users.noreply.github.com>
2024-10-14 21:09:03 +0800

    Update Pharmacist.java

--- user/Pharmacist.java
+++ user/Pharmacist.java
@@ -1,7 +1,37 @@
-package user;
+package JavaProject;
 
-public class Pharmacist extends Staff {
-    public Pharmacist(String id, String name, String role, String gender, int age) {
-        super(id, name, role, gender, age);
+import java.util.List;
+
+public class Pharmacist extends User {
+    private int pharmacistID;
+    public Inventory inventory;
+
+    // Constructor to initialize the pharmacistID and inventory
+    public Pharmacist(int pharmacistID, Inventory inventory) {
+        this.pharmacistID = pharmacistID;
+        this.inventory = inventory;
     }
+
+    public int getPharmacistID() {
+        return pharmacistID;
+    }
+
+    // Haven't finished yet implementing the methods
+    
+    public void viewPrescription() {
+        System.out.println("Viewing prescriptions...");
+    }
+
+    public void updatePrescriptionStatus() {
+        System.out.println("Updating prescription status...");
+    }
+
+    public void monitorInventory() {
+        System.out.println("Monitoring inventory...");
+    }
+
+    public void requestReplenishment() {
+        System.out.println("Requesting replenishment...");
+    }
+
 }
